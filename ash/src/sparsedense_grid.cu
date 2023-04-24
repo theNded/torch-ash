2823344ecec7b176d25d9148032af2b6c7044ee7
Wei Dong <thended@gmail.com>
2023-04-24 12:46:11 -0400

    use template functor

--- ash/src/sparsedense_grid.cu
+++ ash/src/sparsedense_grid.cu
@@ -4,16 +4,54 @@
 #include "minivec.h"
 #include "sparsedense_grid.h"
 
-__device__ __inline__ float smoothstep_fn(float x) { return x*x*(3 - 2 * x);}
-__device__ __inline__ double smoothstep_fn(double x) { return x*x*(3 - 2 * x);}
+template <typename scalar_t>
+class SmoothStepFunctor {
+public:
+    using scalar_type = scalar_t;
+    __device__ __inline__ scalar_t operator()(const scalar_t& x) const {
+        return x * x * (3 - 2 * x);
+    }
+};
 
-__device__ __inline__ double dsmoothstep_dx_fn(double x) { return 6*x*(1 - x);}
-__device__ __inline__ float dsmoothstep_dx_fn(float x) { return 6*x*(1 - x);}
+template <typename scalar_t>
+class DiffSmoothStepFunctor {
+public:
+    using scalar_type = scalar_t;
+    __device__ __inline__ scalar_t operator()(const scalar_t& x) const {
+        return 6 * x * (1 - x);
+    }
+};
 
-const float interp_sum_weight_eps = 0.5;
+template <typename scalar_t>
+class LinearFunctor {
+public:
+    using scalar_type = scalar_t;
+    __device__ __inline__ scalar_t operator()(const scalar_t& x) const {
+        return x;
+    }
+};
+
+template <typename scalar_t>
+class DiffLinearFunctor {
+public:
+    using scalar_type = scalar_t;
+    __device__ __inline__ scalar_t operator()(const scalar_t& x) const {
+        return 1;
+    }
+};
+
+template <typename Functor>
+struct functor_t {
+    __host__ __device__ typename Functor::scalar_type operator()(
+            const typename Functor::scalar_type& x) const {
+        return Functor()(x);
+    }
+};
+
+const float kInterpSumWeightThreshold = 0.5;
 // Now only dispatch dtypes, all the queries are for 3D
 // TODO: dispatch for 2D/4D later
-template <typename scalar_t>
+template <typename InterpFunctor, typename scalar_t>
 __global__ void query_forward_kernel(
         const scalar_t* __restrict__ embeddings,
         const MiniVec<float, 3>* __restrict__ offsets,
@@ -32,6 +70,8 @@ __global__ void query_forward_kernel(
     if (i >= len || !masks[i]) {
         return;
     }
+
+    const functor_t<InterpFunctor> interp_fn;
 
     int grid_idx = grid_indices[i];
     int cell_idx = cell_indices[i];
@@ -58,7 +98,7 @@ __global__ void query_forward_kernel(
         for (int d = 0; d < 3; ++d) {
             int dim_code = (cell_nb >> d) & 1;
             scalar_t x = (dim_code) ? (offset[d]) : (1 - offset[d]);
-            weight *= smoothstep_fn(x);
+            weight *= interp_fn(x);
         }
 
         int cell_nb_idx = neighbor_cell2cell[cell_nb];
@@ -70,7 +110,7 @@ __global__ void query_forward_kernel(
         sum_weight += weight;
     }
 
-    if (sum_weight < interp_sum_weight_eps) {
+    if (sum_weight < kInterpSumWeightThreshold) {
         return;
     }
 
@@ -102,7 +142,8 @@ at::Tensor query_forward(
     at::Tensor output = at::zeros({len, embedding_dims}, embeddings.options());
 
     AT_DISPATCH_FLOATING_TYPES(embeddings.scalar_type(), "query_forward", [&] {
-        query_forward_kernel<scalar_t><<<blocks, threads>>>(
+        using InterpFunctor = SmoothStepFunctor<scalar_t>;
+        query_forward_kernel<InterpFunctor, scalar_t><<<blocks, threads>>>(
                 embeddings.data_ptr<scalar_t>(),
                 static_cast<MiniVec<float, 3>*>(offsets.data_ptr()),
                 grid_indices.data_ptr<int64_t>(),
@@ -121,7 +162,7 @@ at::Tensor query_forward(
     return output;
 }
 
-template <typename scalar_t>
+template <typename InterpFunctor, typename DiffInterpFunctor, typename scalar_t>
 __global__ void query_backward_forward_kernel(
         const scalar_t* __restrict__ z,
         const scalar_t* __restrict__ embeddings,
@@ -142,6 +183,9 @@ __global__ void query_backward_forward_kernel(
     if (i >= len || !masks[i]) {
         return;
     }
+
+    const functor_t<InterpFunctor> interp_fn;
+    const functor_t<DiffInterpFunctor> diff_interp_fn;
 
     int grid_idx = grid_indices[i];
     int cell_idx = cell_indices[i];
@@ -166,12 +210,12 @@ __global__ void query_backward_forward_kernel(
         for (int d = 0; d < 3; ++d) {
             int dim_code = (cell_nb >> d) & 1;
             scalar_t x = (dim_code) ? (offset[d]) : (1 - offset[d]);
-            scalar_t w = smoothstep_fn(x);
+            scalar_t w = interp_fn(x);
             weight *= w;
         }
         sum_weight += weight;
     }
-    if (sum_weight < interp_sum_weight_eps) {
+    if (sum_weight < kInterpSumWeightThreshold) {
         return;
     }
 
@@ -187,8 +231,8 @@ __global__ void query_backward_forward_kernel(
         for (int d = 0; d < 3; ++d) {
             int dim_code = (cell_nb >> d) & 1;
             scalar_t x = (dim_code) ? (offset[d]) : (1 - offset[d]);
-            scalar_t w = smoothstep_fn(x);
-            scalar_t dw = dsmoothstep_dx_fn(x) * ((dim_code) ? 1 : -1);
+            scalar_t w = interp_fn(x);
+            scalar_t dw = diff_interp_fn(x) * ((dim_code) ? 1 : -1);
 
             weight *= w;
 
@@ -236,7 +280,10 @@ std::tuple<at::Tensor, at::Tensor> query_backward_forward(
 
     AT_DISPATCH_FLOATING_TYPES(
             embeddings.scalar_type(), "query_backward_forward_kernel", [&] {
-                query_backward_forward_kernel<scalar_t><<<blocks, threads>>>(
+                using InterpFunctor = SmoothStepFunctor<scalar_t>;
+                using DiffInterpFunctor = DiffSmoothStepFunctor<scalar_t>;
+                query_backward_forward_kernel<InterpFunctor, DiffInterpFunctor,
+                                              scalar_t><<<blocks, threads>>>(
                         z.data_ptr<scalar_t>(), embeddings.data_ptr<scalar_t>(),
                         static_cast<MiniVec<float, 3>*>(offsets.data_ptr()),
                         grid_indices.data_ptr<int64_t>(),
@@ -258,7 +305,7 @@ std::tuple<at::Tensor, at::Tensor> query_backward_forward(
     return std::make_tuple(grad_embeddings, grad_offsets);
 }
 
-template <typename scalar_t>
+template <typename InterpFunctor, typename DiffInterpFunctor, typename scalar_t>
 __global__ void query_backward_backward_kernel(
         const MiniVec<scalar_t, 3>* __restrict__ grad_grad_offset,
         const scalar_t* __restrict__ z,
@@ -279,6 +326,9 @@ __global__ void query_backward_backward_kernel(
     if (i >= len || !masks[i]) {
         return;
     }
+
+    const functor_t<InterpFunctor> interp_fn;
+    const functor_t<DiffInterpFunctor> diff_interp_fn;
 
     int grid_idx = grid_indices[i];
     int cell_idx = cell_indices[i];
@@ -303,11 +353,11 @@ __global__ void query_backward_backward_kernel(
         for (int d = 0; d < 3; ++d) {
             int dim_code = (cell_nb >> d) & 1;
             scalar_t x = (dim_code) ? (offset[d]) : (1 - offset[d]);
-            weight *= smoothstep_fn(x);
+            weight *= interp_fn(x);
         }
         sum_weight += weight;
     }
-    if (sum_weight < interp_sum_weight_eps) {
+    if (sum_weight < kInterpSumWeightThreshold) {
         return;
     }
 
@@ -322,8 +372,8 @@ __global__ void query_backward_backward_kernel(
         for (int d = 0; d < 3; ++d) {
             int dim_code = (cell_nb >> d) & 1;
             scalar_t x = (dim_code) ? (offset[d]) : (1 - offset[d]);
-            scalar_t w = smoothstep_fn(x);
-            scalar_t dw = dsmoothstep_dx_fn(x) * ((dim_code) ? 1 : -1);
+            scalar_t w = interp_fn(x);
+            scalar_t dw = diff_interp_fn(x) * ((dim_code) ? 1 : -1);
 
             weight_grad[0] *= (d == 0) ? dw : w;
             weight_grad[1] *= (d == 1) ? dw : w;
@@ -374,7 +424,10 @@ std::tuple<at::Tensor, at::Tensor> query_backward_backward(
 
     AT_DISPATCH_FLOATING_TYPES(
             embeddings.scalar_type(), "query_backward_backward", [&] {
-                query_backward_backward_kernel<scalar_t><<<blocks, threads>>>(
+                using InterpFunctor = SmoothStepFunctor<scalar_t>;
+                using DiffInterpFunctor = DiffSmoothStepFunctor<scalar_t>;
+                query_backward_backward_kernel<InterpFunctor, DiffInterpFunctor,
+                                               scalar_t><<<blocks, threads>>>(
                         // unused grad_grad_embeddings.data_ptr<scalar_t>(),
                         static_cast<MiniVec<scalar_t, 3>*>(
                                 grad_grad_offset.data_ptr()),
