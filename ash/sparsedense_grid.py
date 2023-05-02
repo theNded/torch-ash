Crtical bug fixes in backprop (#10)
* optim check ngp dense vs. ash dense

* controlled debugger

* ignore logs

* add summary writer and change learning rate

* random behavior with pure encoding

* backward test for bounded grid

* z-grad fix

* rule-out initialization

* rule-out bounding box

* rule-out divide op

* quick fix to z contiguous

* minor

* clean up debug prints

* update init function signature

* integrate siren net into modules

* update unit test

ash/sparsedense_grid.py
@@ -145,7 +145,7 @@ class SparseDenseGridQuery(torch.autograd.Function):
         ) = ctx.saved_tensors
 
         grad_embeddings, grad_offsets = SparseDenseGridQueryBackward.apply(
-            z,
+            z.contiguous(),
             embeddings,
             offsets,
             grid_indices,
@@ -157,7 +157,19 @@ class SparseDenseGridQuery(torch.autograd.Function):
             ctx.grid_dim,
             ctx.interpolation,
         )
-        return grad_embeddings, grad_offsets, None, None, None, None, None, None, None, None
+
+        return (
+            grad_embeddings,
+            grad_offsets,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
 
 
 class SparseDenseGridQueryBackward(torch.autograd.Function):
@@ -174,17 +186,21 @@ class SparseDenseGridQueryBackward(torch.autograd.Function):
         neighbor_table_cell2cell,
         neighbor_table_cell2grid,
         grid_dim,
-            interpolation,
+        interpolation,
     ):
         """
         Forward pass of the backward function.
         Args:
             z: (num_queries, embedding_dim) gradient of the output w.r.t. y
-            z could be the gradient of the loss w.r.t. y, i.e.,
+
+            z could be a tensor without gradient.
+            Itself is the gradient of the loss w.r.t. y, i.e.,
                 output = dL/dy * dy/dembeddings, dL/dy * dy/doffsets
                        = dL/dembeddings, dL/doffsets
-            z could also be a all-one tensor to get jvp(x), i.e.,
-                output = dy/dembeddings, dy/doffsets
+
+            z could also be a tensor with gradient when used as the forward pass
+            to compute jvp via torch.autograd.grad.
+            It gradient should be passed back to downstream layers (e.g. MLP).
 
             Since y = \\sum_{i=0}^7 weight(offset)[i] * embeddings[i]
 
@@ -230,7 +246,6 @@ class SparseDenseGridQueryBackward(torch.autograd.Function):
             grid_dim,
             interpolation,
         )
-
         return grad_embeddings, grad_offsets
 
     @staticmethod
@@ -240,6 +255,7 @@ class SparseDenseGridQueryBackward(torch.autograd.Function):
         """Backward pass of the backward function.
         When a gradient is computed in by the backward's forward pass and used to compute a loss, its gradient
         need to be properly back propagated back to embeddings and offsets.
+
         Args:
             z: (num_queries, embedding_dim) gradient of the output w.r.t. y
             grad_grad_embeddings: (num_embeddings, cells_per_grid, embedding_dim) gradient of the embeddings
@@ -248,15 +264,20 @@ class SparseDenseGridQueryBackward(torch.autograd.Function):
             Let
                 w1 = grad_embeddings[i] = z * weight(offset)[i], grad_w1 = grad_grad_embeddings[i]
                 w2 = grad_offsets = (z * embeddings[i]) * grad_weight(offset)[i], grad_w2 = grad_grad_offsets
-            We know that w1 is not used in the forward pass, so we can safely ignore dL/dw1.
+
+        TODO: now grad_grad_embeddings is not used. If feature-grid regularization is needed,
+        we need to add it back. Now we safely ignore grad_w1.
+        TODO: at current, grad_offsets are skipped as offsets are not optimized.
             Then we have
+                grad_z = grad_w1 * dw1/dz + grad_w2 * dw2/dz = grad_w2 * dw2/dz
+                       = (grad_w2 * grad_weight(offset)[i]) * embeddings[i] => (num_queries, embedding_dim)
                 grad_embeddings[i] = grad_w2 * dw1/dembeddings[i]
                                    = (grad_w2  * grad_weight(offset)[i]) * z => (1, num_embeddings)
                 grad_offsets = grad_w2 * dw2/doffsets
                              = (z * embeddings[i]) * grad_w2 * hessian_weight(offset)[i] => (1, 3)
-        TODO: at current, grad_offsets are skipped as offsets are not optimized.
 
         Returns:
+            grad_z: (num_queries, embedding_dim) gradient of the output w.r.t. z
             grad_embeddings: (num_embeddings, cells_per_grid, embedding_dim) gradient of the embeddings
             grad_offsets: (num_queries, 3)
         """
@@ -273,8 +294,7 @@ class SparseDenseGridQueryBackward(torch.autograd.Function):
             neighbor_table_cell2grid,
         ) = ctx.saved_tensors
 
-        # Safely ignore dL_(dLdembedding) as dLdembedding is not used in the forward pass
-        grad_embeddings, grad_offsets = backend.query_backward_backward(
+        grad_z, grad_embeddings, grad_offsets = backend.query_backward_backward(
             grad_grad_embeddings,
             grad_grad_offsets,
             z,
@@ -289,7 +309,19 @@ class SparseDenseGridQueryBackward(torch.autograd.Function):
             ctx.grid_dim,
             ctx.interpolation,
         )
-        return None, grad_embeddings, None, None, None, None, None, None, None, None, None
+        return (
+            grad_z,
+            grad_embeddings,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
 
 
 class SparseDenseGrid(ASHModule):
@@ -486,8 +518,10 @@ class SparseDenseGrid(ASHModule):
             masks: (N, 1) tensor of masks
         """
         x = self.transform_world_to_cell(x)
+        # print(x.min(), x.max())
 
         grid_indices, cell_indices, offsets, masks = self.query(x)
+        # print(grid_indices, cell_indices, offsets, masks)
 
         if interpolation == "nearest":
             return self.embeddings[grid_indices, cell_indices], masks
@@ -841,12 +875,12 @@ class BoundedSparseDenseGrid(SparseDenseGrid):
         self.bbox_min = (
             bbox_min.to(self.device)
             if bbox_min is not None
-            else -1 * torch.ones(3, device=self.device)
+            else -1 * torch.ones(self.in_dim, device=self.device)
         )
         self.bbox_max = (
             bbox_max.to(self.device)
             if bbox_max is not None
-            else torch.ones(3, device=self.device)
+            else torch.ones(self.in_dim, device=self.device)
         )
 
         self.cell_size = (self.bbox_max - self.bbox_min) / (
@@ -859,3 +893,12 @@ class BoundedSparseDenseGrid(SparseDenseGrid):
     def grids_in_bound(self, grid_coords: torch.Tensor) -> torch.Tensor:
         masks = ((grid_coords >= 0) * (grid_coords < self.sparse_grid_dim)).all(dim=-1)
         return grid_coords[masks]
+
+    def dense_init_(self):
+        grid_coords = (
+            torch.stack(torch.meshgrid(*[torch.arange(self.sparse_grid_dim)] * self.in_dim))
+            .reshape(self.in_dim, -1)
+            .T.to(self.device)
+        )
+
+        self.engine.insert_keys(grid_coords)

demo/mesh_util.py
@@ -10,132 +10,10 @@ from torch import nn
 import torch.nn.functional as F
 from einops import rearrange
 
-# helpers
-
-
-def exists(val):
-    return val is not None
-
-
-def cast_tuple(val, repeat=1):
-    return val if isinstance(val, tuple) else ((val,) * repeat)
-
-
-# sin activation
-
-
-class Sine(nn.Module):
-    def __init__(self, w0=1.0):
-        super().__init__()
-        self.w0 = w0
-
-    def forward(self, x):
-        return torch.sin(self.w0 * x)
-
-
-# siren layer
-
-
-class Siren(nn.Module):
-    def __init__(
-        self,
-        dim_in,
-        dim_out,
-        w0=1.0,
-        c=6.0,
-        is_first=False,
-        use_bias=True,
-        activation=None,
-    ):
-        super().__init__()
-        self.dim_in = dim_in
-        self.is_first = is_first
-
-        weight = torch.zeros(dim_out, dim_in)
-        bias = torch.zeros(dim_out) if use_bias else None
-        self.init_(weight, bias, c=c, w0=w0)
-
-        self.weight = nn.Parameter(weight)
-        self.bias = nn.Parameter(bias) if use_bias else None
-        self.activation = Sine(w0) if activation is None else activation
-
-    def init_(self, weight, bias, c, w0):
-        dim = self.dim_in
-
-        w_std = (1 / dim) if self.is_first else (math.sqrt(c / dim) / w0)
-        weight.uniform_(-w_std, w_std)
-
-        if exists(bias):
-            bias.uniform_(-w_std, w_std)
-
-    def forward(self, x):
-        out = F.linear(x, self.weight, self.bias)
-        out = self.activation(out)
-        return out
-
-
-# siren network
-
-
-class SirenNet(nn.Module):
-    def __init__(
-        self,
-        dim_in,
-        dim_hidden,
-        dim_out,
-        num_layers,
-        w0=1.0,
-        w0_initial=30.0,
-        use_bias=True,
-        final_activation=None,
-    ):
-        super().__init__()
-        self.num_layers = num_layers
-        self.dim_hidden = dim_hidden
-
-        self.layers = nn.ModuleList([])
-        for ind in range(num_layers):
-            is_first = ind == 0
-            layer_w0 = w0_initial if is_first else w0
-            layer_dim_in = dim_in if is_first else dim_hidden
-
-            self.layers.append(
-                Siren(
-                    dim_in=layer_dim_in,
-                    dim_out=dim_hidden,
-                    w0=layer_w0,
-                    use_bias=use_bias,
-                    is_first=is_first,
-                )
-            )
-
-        final_activation = (
-            nn.Identity() if not exists(final_activation) else final_activation
-        )
-        self.last_layer = Siren(
-            dim_in=dim_hidden,
-            dim_out=dim_out,
-            w0=w0,
-            use_bias=use_bias,
-            activation=final_activation,
-        )
-
-    def forward(self, x, mods=None):
-        mods = cast_tuple(mods, self.num_layers)
-
-        for layer, mod in zip(self.layers, mods):
-            x = layer(x)
-
-            if exists(mod):
-                x *= rearrange(mod, "d -> () d")
-
-        return self.last_layer(x)
-
-
+# temporary helpers
+# TODO: replace them with versatile torch-ash MarchingCubes
 """From the DeepSDF repository https://github.com/facebookresearch/DeepSDF
 """
-
-
 def create_mesh(decoder, filename, N=256, max_batch=64**3, offset=None, scale=None):
     start = time.time()
     ply_filename = filename
@@ -143,8 +21,9 @@ def create_mesh(decoder, filename, N=256, max_batch=64**3, offset=None, scale=No
     # decoder.eval()
 
     # NOTE: the voxel_origin is actually the (bottom, left, down) corner, not the middle
-    voxel_origin = [-1, -1, -1]
-    voxel_size = 2.0 / (N - 1)
+    eps = 1e-2
+    voxel_origin = [-1 + eps, -1 + eps, -1 + eps]
+    voxel_size = 2.0 * (1 - eps) / (N - 1)
 
     overall_index = torch.arange(0, N**3, 1, out=torch.LongTensor())
     samples = torch.zeros(N**3, 4)
@@ -269,39 +148,3 @@ def convert_sdf_samples_to_ply(
     )
 
 
-class SirenMLP(nn.Module):
-    def __init__(self, in_dim, out_dim, hidden_dim, num_layers, device):
-        super().__init__()
-        self.net = SirenNet(
-            dim_in=in_dim,
-            dim_hidden=hidden_dim,
-            dim_out=out_dim,
-            num_layers=num_layers,
-            w0=30.0,
-        ).to(device)
-
-    def forward(self, x):
-        x.requires_grad_(True)
-
-        sdf = self.net(x)
-        grad_x = torch.autograd.grad(
-            outputs=sdf[..., 0],
-            inputs=x,
-            grad_outputs=torch.ones_like(sdf[..., 0], requires_grad=False),
-            create_graph=True,
-            retain_graph=True,
-            only_inputs=True,
-        )[0]
-
-        return sdf, grad_x, torch.ones_like(sdf, dtype=torch.bool).squeeze()
-
-    def spatial_init_(self, x):
-        # TOOD(wei): initialize a sparse-dense grid only for sampling
-        pass
-
-    def sample(self, num_samples):
-        sample_coords = np.random.uniform(-1, 1, size=(num_samples, 3))
-        return torch.from_numpy(sample_coords).float().cuda()
-
-    def marching_cubes(self, fname):
-        create_mesh(self.net, fname, N=256, max_batch=64**3, offset=None, scale=None)

unittests/test_sparsedense_grid.py
@@ -1,10 +1,20 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 import os
-from ash import SparseDenseGrid, SparseDenseGridQuery, SparseDenseGridQueryBackward
+from ash import (
+    SparseDenseGrid,
+    SparseDenseGridQuery,
+    SparseDenseGridQueryBackward,
+    BoundedSparseDenseGrid,
+    SirenNet,
+)
 import pytest
+
+np.random.seed(15213)
+torch.manual_seed(15213)
 
 
 # TODO: now only 3 dim keys is supported for sparse-dense grids
@@ -163,7 +173,8 @@ class TestSparseDenseGrid:
             )
 
         # Map query to [min, max - 1) to check purely in-bound queries
-        query_cell_coords = torch.rand(10, 3, device=self.device)
+        num_queries = 1000
+        query_cell_coords = torch.rand(num_queries, in_dim, device=self.device)
         # min: -grid_dim * bound
         # max: grid_dim * bound - 1
         query_cell_coords = (
@@ -209,12 +220,17 @@ class TestSparseDenseGrid:
         query_positions = query_cell_coords + query_cell_offsets
         query_positions.requires_grad_(True)
 
-        def grad_x_fn(x):
-            x = grid.transform_world_to_cell(x)
+        net = SirenNet(
+            embedding_dim + 3, dim_hidden=128, dim_out=1, num_layers=2, w0=30.0
+        ).cuda()
+
+        grid.construct_sparse_neighbor_tables_()
+
+        def grad_x_fn(x_input):
+            x_input.requires_grad_(True)
+            x = grid.transform_world_to_cell(x_input)
             grid_indices, cell_indices, offsets, masks = grid.query(x)
             assert masks.all()
-
-            grid.construct_sparse_neighbor_tables_()
 
             output = SparseDenseGridQuery.apply(
                 grid.embeddings,
@@ -226,11 +242,22 @@ class TestSparseDenseGrid:
                 grid.neighbor_table_cell2cell,
                 grid.neighbor_table_cell2grid,
                 grid.grid_dim,
+                "linear",
             )
+            output = net(torch.cat((output, x_input), dim=-1))
             return output
 
+        torch.autograd.gradcheck(
+            grad_x_fn,
+            (query_positions,),
+            eps=eps,
+            atol=1e-2,
+            rtol=1e-2,
+        )
+
         def grad_embedding_fn(embeddings):
-            x = grid.transform_world_to_cell(query_positions)
+            x_input = query_positions
+            x = grid.transform_world_to_cell(x_input)
             grid_indices, cell_indices, offsets, masks = grid.query(x)
             assert masks.all()
 
@@ -247,22 +274,17 @@ class TestSparseDenseGrid:
                 grid.neighbor_table_cell2grid,
                 grid.grid_dim,
             )
+            output = net(torch.cat((output, x_input), dim=-1))
             return output
 
-        torch.autograd.gradcheck(
-            grad_x_fn,
-            (query_positions,),
-            eps=eps,
-            atol=1e-3,
-            rtol=1e-3,
-        )
         torch.autograd.gradcheck(
             grad_embedding_fn,
             (grid.embeddings,),
             eps=eps,
-            atol=1e-3,
-            rtol=1e-3,
+            atol=1e-2,
+            rtol=1e-2,
         )
+
 
     def _backward_backward_block(self, in_dim, embedding_dim, grid_dim):
         grid = SparseDenseGrid(
@@ -360,14 +382,12 @@ class TestSparseDenseGrid:
         self._query_block(in_dim=3, embedding_dim=16, grid_dim=8)
 
     def test_forward(self):
-        self._forward_block(in_dim=3, embedding_dim=3, grid_dim=5)
+        self._forward_block(in_dim=3, embedding_dim=3, grid_dim=1)
+        self._forward_block(in_dim=3, embedding_dim=3, grid_dim=4)
+        self._forward_block(in_dim=3, embedding_dim=3, grid_dim=8)
 
     def test_backward(self):
-        self._backward_block(in_dim=3, embedding_dim=1, grid_dim=1)
+        self._backward_block(in_dim=3, embedding_dim=2, grid_dim=1)
         self._backward_block(in_dim=3, embedding_dim=1, grid_dim=2)
-        self._backward_block(in_dim=3, embedding_dim=1, grid_dim=4)
-        self._backward_block(in_dim=3, embedding_dim=3, grid_dim=8)
-
-    def test_backward_backward(self):
-        self._backward_backward_block(in_dim=3, embedding_dim=1, grid_dim=1)
-        self._backward_backward_block(in_dim=3, embedding_dim=3, grid_dim=2)
+        self._backward_block(in_dim=3, embedding_dim=4, grid_dim=4)
+        self._backward_block(in_dim=3, embedding_dim=8, grid_dim=8)