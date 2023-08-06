15a1b964c33ee6e1b2662756f58cb266d16c81a6
Wei Dong <thended@gmail.com>
2023-08-06 13:15:01 -0700

    move stdgpu to submodule

--- ext/stdgpu/scripts/utils/build_documentation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# Build documentation
-cmake -E cmake_echo_color --blue ">>>>> Build documentation"
-cmake --build build --target stdgpu_doc
--- ext/stdgpu/scripts/utils/download_thrust.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-cmake -E cmake_echo_color --blue ">>>>> Download thrust"
-cmake -E chdir external git clone https://github.com/NVIDIA/thrust
-cmake -E chdir external/thrust git fetch --all --tags --prune
-cmake -E chdir external/thrust git checkout tags/1.17.2
